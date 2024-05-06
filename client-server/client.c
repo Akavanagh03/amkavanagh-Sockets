c96202c8da3f229cec41a4ecc9d3aa1a5dd93151
AK <akavanagh003@gmail.com>
2024-05-06 14:17:43 -0600

    added client-server files

--- client-server/client.c
+++ client-server/client.c
@@ -1,7 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
-int main()
+void echomode(int sock,char buffer[BUFFER_SIZE],int val);
+void filemode(int val, char buf[BUFFER_SIZE], int sock);
+
+int main() 
 {
+// socket setup start
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    int option;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    {
+        perror("Socket creation error");
+        return -1;
+    }
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) 
+    {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    {
+        perror("Connection Failed");
+        return -1;
+    }
+// socket setup end
+
+    printf("Enter 1 for echo mode, 2 for file mode: ");
+    scanf("%d", &option);
+    send(sock, &option, sizeof(option), 0);
+
+    if (option == 1) 
+    {
+        // Echo mode
+        echomode(sock,buffer,valread);
+
+    }
+    else if (option == 2) 
+    {
+        // File mode
+        filemode(valread,buffer,sock);
+    }
+
+    close(sock);
 
     return 0;
+}
+
+void echomode(int sock,char buffer[BUFFER_SIZE],int val)
+{
+    while (1) 
+        {
+            printf("Enter message: ");
+            fgets(buffer, BUFFER_SIZE, stdin);
+            send(sock, buffer, strlen(buffer), 0);
+            if (strncmp(buffer, "quit", 4) == 0) {
+                break;
+            }
+            memset(buffer, 0, BUFFER_SIZE);
+            val = read(sock, buffer, BUFFER_SIZE);
+            printf("Server: %s", buffer);
+        }
+}
+void filemode(int val, char buf[BUFFER_SIZE], int sock)
+{
+    FILE *fp = fopen("received_file.txt", "w");
+    if (fp == NULL) 
+    {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((val = read(sock, buf, BUFFER_SIZE)) > 0) 
+    {
+        fwrite(buf, 1, val, fp);
+    }
+
+        fclose(fp);
+        printf("File received successfully.\n");
 }
--- client-server/server.c
+++ client-server/server.c
@@ -1,7 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void filemode(int val,char buf[BUFFER_SIZE],int socket);
+void echomode(int val, char buf[BUFFER_SIZE],int socket);
 
 
-int main()
+int main() 
 {
+//start socket setup
+    int server_socket, client_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    if (bind(server_socket, (struct sockaddr *)&address, sizeof(address)) < 0) 
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_socket, 3) < 0) 
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((client_socket = accept(server_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) 
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+//end of socket set up
+
+    int option;
+    valread = read(client_socket, &option, sizeof(option));
+
+    if (option == 1) 
+    {
+        // Echo mode
+        echomode(valread,buffer,client_socket);
+    } 
+    else if (option == 2) 
+    {
+        // File mode
+        filemode(valread,buffer,client_socket);
+    }
+
+    close(client_socket);
+    close(server_socket);
 
     return 0;
+}
+
+void filemode(int val,char buf[BUFFER_SIZE],int socket)
+{
+    FILE *fp = fopen("sendfile.txt", "r");
+        if (fp == NULL) {
+            perror("Error opening file");
+            exit(EXIT_FAILURE);
+        }
+
+        while ((val = fread(buf, 1, BUFFER_SIZE, fp)) > 0) {
+            send(socket, buf, val, 0);
+        }
+
+        fclose(fp);
+}
+void echomode(int val, char buf[BUFFER_SIZE],int socket)
+{
+    while (1) {
+            val = read(socket, buf, BUFFER_SIZE);
+            if (val <= 0) {
+                break;
+            }
+            printf("Client: %s", buf);
+            send(socket, buf, val, 0);
+            memset(buf, 0, BUFFER_SIZE);
+        }
 }
